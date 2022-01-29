f9727be653525aa182c0687c8ad593d14274f812
Alexandr Nastychenko <asn14072020@gmail.com>
2022-01-29 15:56:08 +0600

    Added animation in the block “Our mission and vision”

--- src/components/main/__opportunities/__mission/main__mission.js
+++ src/components/main/__opportunities/__mission/main__mission.js
@@ -2,23 +2,28 @@ import React from 'react';
 import Info from '../../../common/info/info';
 import PictureMissionBackground from '../../../common/picture/__opportunities/__mission/picture__mission-background';
 import PictureMissionIPhoneX from '../../../common/picture/__opportunities/__mission/picture__mission-iPhoneX';
+import useOnScreen from '../../../../hooks/use-on-screen';
 import './main__mission.css'
 
+
+
 const Mission = () => {
+    const [missionRef, textRef, imgRef] = useOnScreen({ threshold: '0.5' });
+    
     return (
-        <article className='main__mission mission'>
+        <article className='main__mission mission' ref={missionRef}>
             <div className='mission__container _container'>
                 <div className='mission__body'>
-                    <div className='mission__text'>
+                    <div className='mission__text' ref={textRef}>
                         < Info 
                         title={"Our mission and vision"}
                         description={"While technology is our sweet spot, we’ve developed our vision alongside our customers to ensure your everyday problems are solved through simplistic tools. And as your business grows, \n we’ll grow right alongside with you. "} />
                     </div>
                 </div>
+                <div className='mission__img' ref={imgRef}>
+                    < PictureMissionIPhoneX />
+                </div>
             </div>
-            <div className='mission__img'>
-                        < PictureMissionIPhoneX />
-                    </div>
             <div className='mission__background'>
                 < PictureMissionBackground />
             </div>
